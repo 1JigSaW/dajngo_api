2504798aff92b93269f6d359548bc40d28e85421
JigSaW <misho_1998@mail.ru>
2021-11-09 17:47:15 +0300

    add permissions

---
+++ blogapi/posts/permissions.py
@@ -0,0 +1,28 @@
+from rest_framework import permissions
+
+
+class BasePermission(object):
+	"""
+	A base class from which all permission classes should inherit
+	"""
+
+	def has_permission(self, request, view):
+		"""
+		Return `True` if permission is granted, `False` otherwise.
+		"""
+		return True
+
+	def has_object_permission(self, request, view, obj):
+		"""
+		Return `True` if permission is granted, 'False' otherwise
+		"""
+		return True
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+
+	def has_object_permission(self, request, view, obj):
+		if request.method in permissions.SAFE_METHODS:
+			return True
+
+		return obj.author == request.user
